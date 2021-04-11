d43f08e59fd7c6fc9f26b377cd0a3b538fe0f301
amiranda9112 <andreselfm@gmail.com>
2021-04-11 02:30:49 -0500

    tweets

--- UranoApi/feed/views.py
+++ UranoApi/feed/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 
+from .models import Tweet
+
 # Create your views here.
-tweets = [{'name': 'ami', 'text': 'This is my first tweet'},
-          {'name': 'randa', 'text': 'This is my second tweet'}]
+
 
 
 def home(request):
-    context = {'tweets': tweets}
+    context = {'tweets': Tweet.objects.all}
     return render(request, 'feed/home.html', context)
